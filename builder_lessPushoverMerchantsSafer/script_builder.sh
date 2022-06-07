61070e1d2c7fe6853c57e10d7415d240771e94f2
otikscypi <otikscypi@gmail.com>
2022-06-08 01:37:33 +0200

    Script builders and initial set of scripts

---
+++ builder_lessPushoverMerchantsSafer/script_builder.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+TEMPLATE_DIR=./src/script_templates
+HEADER_TEMPLATE=$TEMPLATE_DIR/script_header
+BODY_TEMPLATE=$TEMPLATE_DIR/script_body
+FOOTER_TEMPLATE=$TEMPLATE_DIR/script_footer
+
+LIST_FILE=../actor_lists/trader_list
+readarray -t trainerListLines < $LIST_FILE
+
+TEMP_FILE=./tmp/script_tmp
+OUTPUT=../output/lessPushoverMerchantsSafer
+
+echo "Running Merchants (safer) script builder"
+
+touch $OUTPUT
+
+cat $HEADER_TEMPLATE > $OUTPUT
+
+for LIST_LINE in "${trainerListLines[@]}"
+do
+    echo $LIST_LINE
+    touch $TEMP_FILE
+    cp $BODY_TEMPLATE $TEMP_FILE
+    sed -i -e "s/#ACTOR#/$LIST_LINE/g" $TEMP_FILE
+    cat $TEMP_FILE >> $OUTPUT
+    rm $TEMP_FILE
+done
+
+cat $FOOTER_TEMPLATE >> $OUTPUT
---
+++ runAllBuilders.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd ./builder_lessPushoverEnchantersSafer
+./script_builder.sh
+cd ../builder_lessPushoverMerchantsSafer
+./script_builder.sh
+cd ../builder_lessPushoverMerchantsStronger
+./script_builder.sh
+cd ../builder_lessPushoverTrainers
+./script_builder.sh
