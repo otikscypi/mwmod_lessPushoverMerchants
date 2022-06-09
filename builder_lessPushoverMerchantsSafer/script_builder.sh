85a6d70a2a82a6d61de758cf68836c7313ff74ce
otikscypi <otikscypi@gmail.com>
2022-06-09 03:45:29 +0200

    Version 1.1: bugfixes + Tamriel Rebuilt

--- builder_lessPushoverMerchantsSafer/script_builder.sh
+++ builder_lessPushoverMerchantsSafer/script_builder.sh
@@ -1,34 +1,56 @@
 #!/bin/bash
 
 TEMPLATE_DIR=./src/script_templates
+README_TEMPLATE=../script_readme_template
 HEADER_TEMPLATE=$TEMPLATE_DIR/script_header
 BODY_TEMPLATE=$TEMPLATE_DIR/script_body
 FOOTER_TEMPLATE=$TEMPLATE_DIR/script_footer
 
-LIST_FILE=../actor_lists/trader_list
-readarray -t trainerListLines < $LIST_FILE
+masters_arr=("Vanilla" "TamrielRebuilt")
 
-TEMP_FILE=./tmp/script_tmp
-OUTPUT_DIR=../output
-OUTPUT_FILE=$OUTPUT_DIR/lessPushoverMerchantsSafer
-
-echo "Running Merchants (safer) script builder"
-
-if [[ ! -d $OUTPUT_DIR ]]; then
-    mkdir $OUTPUT_DIR
-fi
-touch $OUTPUT_FILE
-
-cat $HEADER_TEMPLATE > $OUTPUT_FILE
-
-for LIST_LINE in "${trainerListLines[@]}"
+for MASTER in "${masters_arr[@]}"
 do
-    echo $LIST_LINE
-    touch $TEMP_FILE
-    cp $BODY_TEMPLATE $TEMP_FILE
-    sed -i -e "s/#ACTOR#/$LIST_LINE/g" $TEMP_FILE
-    cat $TEMP_FILE >> $OUTPUT_FILE
-    rm $TEMP_FILE
-done
+    SCRIPT_PREFIX=otk_
 
-cat $FOOTER_TEMPLATE >> $OUTPUT_FILE
+    LIST_FILE=../actor_lists/$MASTER/trader_list
+    readarray -t trainerListLines < $LIST_FILE
+
+    TEMP_FILE=./tmp/script_tmp
+    OUTPUT_DIR=../output
+    OUTPUT_FILE=$OUTPUT_DIR/$MASTER/lessPushoverMerchantsSafer
+
+    SCRIPT_SUFFIX=""
+    if [[ "$MASTER" == TamrielRebuilt ]]; then
+        SCRIPT_SUFFIX="_TR"
+    fi
+
+    echo "Running Merchants (safer) script builder"
+
+    if [[ ! -d $OUTPUT_DIR ]]; then
+        mkdir $OUTPUT_DIR
+    fi
+
+    if [[ ! -d $OUTPUT_DIR/$MASTER ]]; then
+        mkdir $OUTPUT_DIR/$MASTER
+    fi
+
+    touch $OUTPUT_FILE
+
+    cat $README_TEMPLATE > $OUTPUT_FILE
+    cat $HEADER_TEMPLATE >> $OUTPUT_FILE
+
+    for LIST_LINE in "${trainerListLines[@]}"
+    do
+        echo $LIST_LINE
+        touch $TEMP_FILE
+        cp $BODY_TEMPLATE $TEMP_FILE
+        sed -i -e "s/#ACTOR#/$LIST_LINE/g" $TEMP_FILE
+        cat $TEMP_FILE >> $OUTPUT_FILE
+        rm $TEMP_FILE
+    done
+
+    cat $FOOTER_TEMPLATE >> $OUTPUT_FILE
+
+    sed -i -e "s/#SCRIPT_PREFIX#/$SCRIPT_PREFIX/g" $OUTPUT_FILE
+    sed -i -e "s/#SCRIPT_SUFFIX#/$SCRIPT_SUFFIX/g" $OUTPUT_FILE
+done
